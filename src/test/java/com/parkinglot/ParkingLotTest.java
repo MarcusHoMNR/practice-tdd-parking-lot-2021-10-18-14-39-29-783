2d3174f92395af33351481e4f3e272fdf956ba42
Marcus Ho <marcus.ho@oocl.com>
2021-12-03 14:21:02 +0800

    [TTDOOP practice][Marcus]feat: testcase fetch single success

--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -38,15 +38,15 @@ public class ParkingLotTest {
     void should_return_target_car_when_fetchCar_given_customer_with_one_valid_ticket() {
         //given
 
-        Car car = new Car("car001");
+        Car targetCar = new Car("car001");
         Ticket ticket = new Ticket(car.getCarId());
         Customer customer = new Customer(ticket);
         ParkingLot parkingLot = new ParkingLot();
 
         //when
-        Car targetCar = parkingLot.fetchCar(car);
+        Car fetchedCar = parkingLot.fetchCar(targetCar);
 
         //then
-        assertNotNull(targetCar);
+        assertEquals(targetCar, fetchedCar);
     }
 }
