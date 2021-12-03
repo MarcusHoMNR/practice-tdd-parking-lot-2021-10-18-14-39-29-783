33900a114af425b73e64aece41252821560855ac
Marcus Ho <marcus.ho@oocl.com>
2021-12-03 18:08:08 +0800

    [TTDOOP practice][Marcus]feat:  SuperSmartparkingboy handle two second more mt spaces rate

--- src/test/java/com/parkinglot/SuperSmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SuperSmartParkingBoyTest.java
@@ -30,4 +30,26 @@ public class SuperSmartParkingBoyTest {
                 () -> assertTrue(parkingLot2.getParkedCarList().isEmpty())
         );
     }
+
+    @Test
+    void should_park_in_second_lot_when_parkCars_given_super_smart_parking_boy_manage_two_parking_lot_second_more_mt_space_rate_and_car_and_customer() {
+        //given
+        ParkingLot parkingLot1 = new ParkingLot();
+        ParkingLot parkingLot2 = new ParkingLot(15);
+
+        SuperSmartParkingBoy superSmartParkingBoy  = new SuperSmartParkingBoy(new ArrayList<>(Arrays.asList(parkingLot1, parkingLot2)));
+        Customer customer = new Customer();
+        Car targetCar = new Car("car001");
+
+        //when
+        superSmartParkingBoy.parkCars(customer, new ArrayList<>(Collections.singletonList(targetCar)));
+
+        //then
+        assertAll(
+                () -> assertFalse(parkingLot2.getParkedCarList().isEmpty()),
+                () -> assertEquals(1, parkingLot2.getParkedCarList().size()),
+                () -> assertEquals(targetCar, parkingLot2.getParkedCarList().get(0)),
+                () -> assertTrue(parkingLot1.getParkedCarList().isEmpty())
+        );
+    }
 }
