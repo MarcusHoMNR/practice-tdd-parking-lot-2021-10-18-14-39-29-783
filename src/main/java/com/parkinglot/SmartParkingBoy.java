fd8454344536b09a1cadc7e05d0dcc554149b6dd
Marcus Ho <marcus.ho@oocl.com>
2021-12-03 19:03:46 +0800

    [TTDOOP practice][Marcus]feat:  testcase Smartparkingboy park full

--- src/main/java/com/parkinglot/SmartParkingBoy.java
+++ src/main/java/com/parkinglot/SmartParkingBoy.java
@@ -1,42 +1,19 @@
 package com.parkinglot;
 
-import com.parkinglot.Exception.UnrecognizedTicketExpection;
-
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static java.util.Comparator.comparingInt;
 
-public class SmartParkingBoy {
-    final List<ParkingLot> parkingLotList;
+public class SmartParkingBoy extends ParkingBoy{
 
     public SmartParkingBoy(List<ParkingLot> parkingLotList) {
-        this.parkingLotList = parkingLotList;
+        super(parkingLotList);
     }
 
+    @Override
     public void parkCars(Customer customer, List<Car> carList) {
         parkingLotList.stream().max(comparingInt(ParkingLot::getRemainingEmptySpacesNumber))
                 .get()
                 .parkCars(customer, carList);
     }
-
-    public List<Car> fetchCars(Customer customer) {
-        if (customer.getTicketList().isEmpty()) {
-            throw new UnrecognizedTicketExpection();
-        }
-        for (Ticket ticket : customer.getTicketList()) {
-            if (ticket.isFetched()) {
-                throw new UnrecognizedTicketExpection();
-            }
-        }
-        ParkingLot targetParkingLot = parkingLotList.stream()
-                .filter(
-                        parkingLot ->
-                                parkingLot.getParkedCarList().stream().map(Car::getCarId).collect(Collectors.toList()).contains(
-                                        customer.getTicketList().get(0).getLinkedCarId()
-                                )
-                ).findAny().get();
-        return targetParkingLot.fetchCarListByCustomer(customer);
-    }
-
 }
