b44ba898c62581ce3429721fd7953e488a54713c
Marcus Ho <marcus.ho@oocl.com>
2021-12-03 14:04:51 +0800

    [TTDOOP practice][Marcus]feat: testcase park full

--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -2,8 +2,6 @@ package com.parkinglot;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ParkingLotTest {
@@ -11,6 +9,19 @@ public class ParkingLotTest {
     void should_return_customer_with_ticket_when_parkCar_given_parking_lot_And_Car() {
         //given
         ParkingLot parkingLot = new ParkingLot();
+        Car car = new Car("car001");
+
+        //when
+        Customer customer = parkingLot.parkCar(car);
+
+        //then
+        assertNotNull(customer.getTicketList());
+    }
+
+    @Test
+    void should_return_null_when_parkCar_given_parking_lot_full_And_Car() {
+        //given
+        ParkingLot parkingLot = new ParkingLot(0);
         Car car = new Car("car001");
 
         //when
