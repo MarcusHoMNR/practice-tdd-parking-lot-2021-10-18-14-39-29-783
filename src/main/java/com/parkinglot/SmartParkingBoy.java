05a52cd776a2b7f1e1455dbdc16a8b81ee376112
Marcus Ho <marcus.ho@oocl.com>
2021-12-03 17:20:53 +0800

    [TTDOOP practice][Marcus]feat:  Smartparkingboy handle two lot same mt spaces

--- src/main/java/com/parkinglot/SmartParkingBoy.java
+++ src/main/java/com/parkinglot/SmartParkingBoy.java
@@ -1,2 +1,12 @@
-package com.parkinglot;public class SmartParkingBoy {
+package com.parkinglot;
+
+import java.util.List;
+
+public class SmartParkingBoy {
+    final List<ParkingLot> parkingLotList;
+    public SmartParkingBoy(List<ParkingLot> parkingLotList) {
+        this.parkingLotList = parkingLotList;
+    }
+
+    public void parkCars(Customer customer, List<Car> carList) {}
 }
--- src/test/java/com/parkinglot/SmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SmartParkingBoyTest.java
@@ -1,4 +1,33 @@
 package com.parkinglot;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class SmartParkingBoyTest {
+    @Test
+    void should_park_in_first_lot_when_parkCars_given_smart_parking_boy_manage_two_parking_lot_same_mt_space_and_car() {
+        //given
+        ParkingLot parkingLot1 = new ParkingLot();
+        ParkingLot parkingLot2 = new ParkingLot();
+
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(new ArrayList<>(Arrays.asList(parkingLot1, parkingLot2)));
+        Customer customer = new Customer();
+        Car targetCar = new Car("car001");
+
+        //when
+        smartParkingBoy.parkCars(customer, new ArrayList<>(Collections.singletonList(targetCar)));
+
+        //then
+        assertAll(
+                () -> assertNotNull(parkingLot1.getParkedCarList()),
+                () -> assertEquals(1, parkingLot1.getParkedCarList().size()),
+                () -> assertEquals(targetCar, parkingLot1.getParkedCarList().get(0)),
+                () -> assertTrue(parkingLot2.getParkedCarList().isEmpty())
+        );
+    }
 }
