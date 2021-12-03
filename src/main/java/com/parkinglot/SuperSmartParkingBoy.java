2f2847af1b59ef640c9ad18ecaf9535acce4c8e5
Marcus Ho <marcus.ho@oocl.com>
2021-12-03 18:05:05 +0800

    [TTDOOP practice][Marcus]feat:  testCase SuperSmartparkingboy handle two same mt spaces rate

--- src/main/java/com/parkinglot/SuperSmartParkingBoy.java
+++ src/main/java/com/parkinglot/SuperSmartParkingBoy.java
@@ -4,17 +4,13 @@ import java.util.List;
 
 import static java.util.Comparator.comparingInt;
 
-public class SmartParkingBoy {
+public class SuperSmartParkingBoy {
     final List<ParkingLot> parkingLotList;
 
-    public SmartParkingBoy(List<ParkingLot> parkingLotList) {
+    public SuperSmartParkingBoy(List<ParkingLot> parkingLotList) {
         this.parkingLotList = parkingLotList;
     }
 
     public void parkCars(Customer customer, List<Car> carList) {
-        parkingLotList.stream().max(comparingInt(ParkingLot::getRemainingEmptySpacesNumber))
-                .get()
-                .parkCars(customer, carList);
     }
-
 }
--- src/test/java/com/parkinglot/SuperSmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SuperSmartParkingBoyTest.java
@@ -1,4 +1,33 @@
 package com.parkinglot;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class SuperSmartParkingBoyTest {
+    @Test
+    void should_park_in_first_lot_when_parkCars_given_super_smart_parking_boy_manage_two_parking_lot_same_mt_space_rate_and_car_and_customer() {
+        //given
+        ParkingLot parkingLot1 = new ParkingLot();
+        ParkingLot parkingLot2 = new ParkingLot();
+
+        SuperSmartParkingBoy superSmartParkingBoy  = new SuperSmartParkingBoy(new ArrayList<>(Arrays.asList(parkingLot1, parkingLot2)));
+        Customer customer = new Customer();
+        Car targetCar = new Car("car001");
+
+        //when
+        superSmartParkingBoy.parkCars(customer, new ArrayList<>(Collections.singletonList(targetCar)));
+
+        //then
+        assertAll(
+                () -> assertFalse(parkingLot1.getParkedCarList().isEmpty()),
+                () -> assertEquals(1, parkingLot1.getParkedCarList().size()),
+                () -> assertEquals(targetCar, parkingLot1.getParkedCarList().get(0)),
+                () -> assertTrue(parkingLot2.getParkedCarList().isEmpty())
+        );
+    }
 }
